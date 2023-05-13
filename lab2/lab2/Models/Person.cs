66e69a363f2cba291a1586b12a68a58753472a1b
Siromanchyk <102350679+Siromanchyk@users.noreply.github.com>
2023-05-13 11:08:00 +0300

    Add files via upload

---
+++ lab2/lab2/Models/Person.cs
@@ -0,0 +1,29 @@
+﻿namespace lab2.Models
+{
+    public class Person
+    {
+        protected string _name = "";
+        protected string _surname = "";
+        protected string _dateOfBirthsday = "";
+
+        public string Name
+        {
+            get { return this._name; }
+            set { this._name = value; }
+        }
+        public string Surname
+        {
+            get { return this._surname; }
+            set { this._surname = value; }
+        }
+        public string DateOfBirthsday
+        {
+            get { return this._dateOfBirthsday; }
+            set { this._dateOfBirthsday = value; }
+        }
+        public virtual string toString()
+        {
+            return $"{this.Name} {this.Surname} {this.DateOfBirthsday}";
+        }
+    }
+}
