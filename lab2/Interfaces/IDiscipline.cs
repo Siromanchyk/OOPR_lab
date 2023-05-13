679d4c672c9f2210f21b6af6a1e81f24594395bc
Siromanchyk <102350679+Siromanchyk@users.noreply.github.com>
2023-05-13 11:06:44 +0300

    Delete lab2 directory

--- lab2/Interfaces/IDiscipline.cs
+++
@@ -1,9 +0,0 @@
-﻿using lab2.Models;
-
-namespace lab2.Interfaces
-{
-    public interface IDiscipline
-    {
-        string Name { get; set; }
-    }
-}
--- lab2/Interfaces/IPerson.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace lab2.Interfaces
-{
-    public interface IPerson
-    {
-        string Name { get; set; }
-        string Surname { get; set; }
-        string DateOfBirthsday { get; set; }
-    }
-}
